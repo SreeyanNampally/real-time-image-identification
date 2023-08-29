b8cf1034ed67b125b481335ac68badbef4412b21
Sreeyan Nampally <98562308+SreeyanNampally@users.noreply.github.com>
2023-08-29 17:23:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier("Mobilenet", modelloaded);
+}
+
+function modelloaded(){
+  console.log("model loaded");
+}
+
+
+function draw(){
+ image(video, 0,0, 300, 300);
+ classifier.classify(video, gotResult);
+}
+
+previous_result = "";
+
+function gotResult(error, results){
+  if (error) {
+    console.log(error);
+  } else {
+    
+    if ((results[0].confidence>0.5)&& previous_result != results[0].label) {
+      console.log(results);
+      previous_result = results[0].label;
+      synth = window.speechSynthesis
+      speakdata = "object detected is " + results[0].label;
+      utterthis = new SpeechSynthesisUtterance(speakdata);
+      synth.speak(utterthis);
+      document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = (results[0].confidence*100).toFixed(2);
+    }
+  }
+}
+
+
