61b3ba77f4ac2b61dc6f44f8919c15710b0bf3a1
Sreeyan Nampally <98562308+SreeyanNampally@users.noreply.github.com>
2023-09-22 16:21:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
-  classifier = ml5.imageClassifier("Mobilenet", modelloaded);
+  classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/R3rBWMPIl/model.json", modelloaded);
 }
 
 function modelloaded(){
